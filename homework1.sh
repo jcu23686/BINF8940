0283e0362e6fb6fe41793a08326dfb5535448a60
jcu23686 <112415240+jcu23686@users.noreply.github.com>
2022-09-26 21:30:50 -0400

    Trying to fix errors with batch script.
    I can get my linux commands to work on the cluster but not in my scripts.

--- homework1.sh
+++ homework1.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=2		                            # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=40gb			                                # Total memory for job
 #SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/work/gene8940/jcu23686			    # Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --output=/work/gene8940/jcu23686/log.%j			    # Location of standard output and error log files (replace cbergman with your myid)
 #SBATCH --mail-user=jcu23686@uga.edu                    # Where to send mail (replace cbergman with your myid)
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
@@ -18,5 +18,5 @@ then
     mkdir -p $OUTDIR
 fi
    
-    cut -f3 ecoli_MG1655.gff|grep -c "CDS" >results.txt         #specifically 3rd column, count CDS in that column
+    cut -f3 ecoli_MG1655.gff|grep -c "CDS">results.txt         #specifically 3rd column, count CDS in that column
 
