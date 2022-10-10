1fc1ced17bd3bba91014f90446491e6a628bac52
jcu23686 <112415240+jcu23686@users.noreply.github.com>
2022-10-10 13:03:37 -0400

    add quast

--- homework3.sh
+++ homework3.sh
@@ -26,3 +26,4 @@ module load QUAST/5.0.2-foss-2019b-Python-3.7.4
 curl -s ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c> e_coli_MG1655.fna
 canu -p ecoli -d $OUTDIR/canu genomeSize=4.8m useGRID=false -pacbio-raw /work/gene8940/jcu23686/homework3/ecoli_p6_25x.filtered.fastq.gz #I copied the files from instructor_data to my work directory
 spades.py -t 6 -k 21,33,55,77 --isolate --memory 40 -1 s_6_1.fastq.gz -2 s_6_2.fastq.gz -o $OUTDIR/spades
+quast.py -t -6 -r e_coli_MG1655.fna $OUTDIR/canu/ecoli.contigs.fasta $OUTDIR/spades/scaffolds.fasta
