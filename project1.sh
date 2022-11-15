0299034e4a130de9747ff0a38c03b91896a2715f
jcu23686 <112415240+jcu23686@users.noreply.github.com>
2022-11-14 20:11:17 -0500

    Script1

--- project1.sh
+++ project1.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=BLAST-test		                        # Job name
 #SBATCH --partition=batch		                            # Partition (queue) name
 #SBATCH --ntasks=1			                                # Single task job
-#SBATCH --cpus-per-task=6		                            # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=40gb			                                # Total memory for job
-#SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --cpus-per-task=10		                            # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=24gb			                                # Total memory for job
+#SBATCH --time=8:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output=/work/gene8940/jcu23686/log.%j			    # Location of standard output and error log files (replace cbergman with your myid)
 #SBATCH --mail-user=jcu23686@uga.edu                    # Where to send mail (replace cbergman with your myid)
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
@@ -19,7 +19,14 @@ then
     mkdir -p $OUTDIR
 fi
 
+cd $OUTDIR
+qlogin
+
+wget https://data.nist.gov/od/ds/ark:/88434/mds2-2578/assemblies-and-benchmarking_results/asm1.tar.gz
+tar -xvzf asm1.tar.gz
+module load QUAST/5.0.2-foss-2019b-Python-3.7.4
+quast asm1/assembly/Ash1v1.7.fa.gz
 
 
-curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz | gunzip -c> human.fna #data in fasta format
-curl -s https://s3-us-west-2.amazonaws.com/human-pangenomics/NHGRI_UCSC_panel/HG002/hpp_HG002_NA24385_son_v1/PacBio_HiFi/15kb/m64012_190920_173625.Q20.fastq > HG002.fastq #data in fastq format
+#curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz | gunzip -c> human.fna #data in fasta format
+#curl -s https://s3-us-west-2.amazonaws.com/human-pangenomics/NHGRI_UCSC_panel/HG002/hpp_HG002_NA24385_son_v1/PacBio_HiFi/15kb/m64012_190920_173625.Q20.fastq > HG002.fastq #data in fastq format
