4aa5bf76ecd325d027cd4923a0d7ad2af2b5027a
jcu23686 <112415240+jcu23686@users.noreply.github.com>
2022-11-27 14:21:48 -0500

    22

--- project1.sh
+++ project1.sh
@@ -160,8 +160,13 @@ module load QUAST/5.0.2-foss-2019b-Python-3.7.4
 #assembly 21
 #wget https://data.nist.gov/od/ds/ark:/88434/mds2-2578/assemblies-and-benchmarking_results/asm21.tar.gz
 #tar -xzvf asm21.tar.gz
-quast asm21/assembly/asm21/HPRC_HG002.hic1_hic2.salsa.fasta
+#quast asm21/assembly/asm21/HPRC_HG002.hic1_hic2.salsa.fasta
 
+#assembly 22
+#wget https://data.nist.gov/od/ds/ark:/88434/mds2-2578/assemblies-and-benchmarking_results/asm22ab.tar.gz
+#tar -xzvf asm22ab.tar.gz
+quast asm22ab/assembly/asm22a/Dovetail_HG002_phase1_scaffolds_with_X_Y.fa.gz
+quast asm22ab/assembly/asm22b/Dovetail_HG002_phase2_scaffolds_with_X_Y.fa.gz
 
 
 #curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz | gunzip -c> human.fna #data in fasta format
