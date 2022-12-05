4374f7d503c9cbe6bd7cdb6019ebcb92926b73dc
jcu23686 <112415240+jcu23686@users.noreply.github.com>
2022-12-05 16:52:21 -0500

    Final change
    Script runs for a long time if everything is uncommented so many of the commands are comment out. Uncomment assemblies that you want to run quast on

--- project1.sh
+++ project1.sh
@@ -24,6 +24,7 @@ qlogin
 
 module load QUAST/5.0.2-foss-2019b-Python-3.7.4
 
+#UNCOMMENT CODE FROM ASSEMBLIES THAT YOU ARE TRYING TO RUN QUAST ON
 #assembly1
 #wget https://data.nist.gov/od/ds/ark:/88434/mds2-2578/assemblies-and-benchmarking_results/asm1.tar.gz
 #tar -xvzf asm1.tar.gz
@@ -189,8 +190,8 @@ module load QUAST/5.0.2-foss-2019b-Python-3.7.4
 #assembly 26
 #wget https://data.nist.gov/od/ds/ark:/88434/mds2-2578/assemblies-and-benchmarking_results/HG002-v0.11.tar.gz
 #tar -xzvf HG002-v0.11.tar.gz
-quast HG002-v0.11/assembly/HG002-v0.11.mat.fa.gz
-quast HG002-v0.11/assembly/HG002-v0.11.pat.fa.gz
+#quast HG002-v0.11/assembly/HG002-v0.11.mat.fa.gz
+#quast HG002-v0.11/assembly/HG002-v0.11.pat.fa.gz
 
 #curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz | gunzip -c> human.fna #data in fasta format
 #curl -s https://s3-us-west-2.amazonaws.com/human-pangenomics/NHGRI_UCSC_panel/HG002/hpp_HG002_NA24385_son_v1/PacBio_HiFi/15kb/m64012_190920_173625.Q20.fastq > HG002.fastq #data in fastq format
